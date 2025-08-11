913f9f2d9a7a7fdb79d971e7ea59dc15b328480a
leonardo_Wer <name89213126414@gmail.com>
2025-08-11 22:56:35 +0300

    Benefits design + scroll trigger anim

--- src/js/main.js
+++ src/js/main.js
@@ -39,16 +39,13 @@ function initBenefits() {
     ];
 
 
-    const opacityFactor = 0.5;
-
     const hpContainerCenter = document.querySelector('.headphones-container__center');
     const wrapper = document.querySelector('.headphones-container-center__wrapper');
 
     // 1. Создание тайлов и добавление в контейнер:
-    const tiles = benefitsData.map(tileData => {
+    benefitsData.map(tileData => {
         const tile = createBenefitTile(tileData);
         wrapper.appendChild(tile);
-        return tile;
     });
 
     const wrapperCopy = wrapper.cloneNode(true);
@@ -57,14 +54,26 @@ function initBenefits() {
     hpContainerCenter.appendChild(wrapperCopy);
 
     // Анимация с GSAP:
-    gsap.to(hpContainerCenter,
+    const fadeInTl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".headphones-container",
+            start: "60% bottom",
+        }
+    })
+    fadeInTl.to(hpContainerCenter,
         {
             xPercent: -98,
-            duration: 15,
+            duration: 20,
             ease: "none",
             repeat: -1,
-        });
+        }, 0)
+        .from(".headphones-container__center-wrapper", {
+            opacity: 0,
+            ease: "power1.in",
+            duration: 0.4
+        }, 0.5)
 }
+
 
 initBenefits();
 
